d0593a5f8850cd0f10274b356996666950e256de
progzc <zcprog@foxmail.com>
2020-12-15 15:27:12 +0800

    最小栈

---
+++ Arithmetic/src/main/java/com/zcprog/arithmetic/LeetCode_0155_MinStack.java
@@ -0,0 +1,83 @@
+package com.zcprog.arithmetic;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @Description 能在常数时间内检索到最小元素的栈
+ * @Author zhaochao
+ * @Date 2020/12/15 14:56
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class LeetCode_0155_MinStack {
+    /**
+     * LeetCode地址：https://leetcode-cn.com/problems/min-stack/
+     * 设计一个支持 push，pop，top操作，并能在常数时间内检索到最小元素的栈。
+     * push(x) —— 将元素 x 推入栈中。
+     * pop() —— 删除栈顶的元素。
+     * top() —— 获取栈顶元素。
+     * getMin() —— 检索栈中的最小元素。
+     * <p>
+     * 示例:
+     * 输入：
+     * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+     * [[],[-2],[0],[-3],[],[],[],[]]
+     * 输出：
+     * [null,null,null,null,-3,null,0,-2]
+     * 解释：
+     * MinStack minStack = new MinStack();
+     * minStack.push(-2);
+     * minStack.push(0);
+     * minStack.push(-3);
+     * minStack.getMin();   --> 返回 -3.
+     * minStack.pop();
+     * minStack.top();      --> 返回 0.
+     * minStack.getMin();   --> 返回 -2.
+     * <p>
+     * 提示：
+     * pop、top 和 getMin 操作总是在 非空栈 上调用。
+     */
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+
+
+    }
+
+    static class MinStack {
+        Deque<Integer> xStack;
+        Deque<Integer> minStack;
+
+        public MinStack() {
+            xStack = new LinkedList<>();
+            minStack = new LinkedList<>();
+            minStack.push(Integer.MAX_VALUE);
+        }
+
+        public void push(int x) {
+            xStack.push(x);
+            minStack.push(Math.min(minStack.peek(), x));
+        }
+
+        public void pop() {
+            xStack.pop();
+            minStack.pop();
+        }
+
+        public int top() {
+            return xStack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+
+    }
+}

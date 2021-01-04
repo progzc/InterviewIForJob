909c0a47506a9f9d1a42b0163b771ad5b4eb97d8
progzc <zcprog@foxmail.com>
2021-01-04 10:53:05 +0800

    Map集合不安全及解决办法

--- JVM/src/test/java/org/fenixsoft/jvm/ContainerNotSafeDemo.java
+++ JVM/src/test/java/org/fenixsoft/jvm/ContainerNotSafeDemo.java
@@ -19,7 +19,7 @@ public class ContainerNotSafeDemo {
     public static void main(String[] args) {
         listNotSafe();
         setNoSafe();
-//        mapNotSafe();
+        mapNotSafe();
     }
 
     private static void mapNotSafe() {

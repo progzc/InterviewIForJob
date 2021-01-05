153fd9082e822cfd5e3df4c931d4bf5bbfb0e9db
progzc <zcprog@foxmail.com>
2021-01-05 21:19:02 +0800

    jinfo的练习使用

--- JVM/src/test/java/org/fenixsoft/jvm/HelloGC.java
+++ JVM/src/test/java/org/fenixsoft/jvm/HelloGC.java
@@ -1,5 +1,7 @@
 package org.fenixsoft.jvm;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Description 通过程序获得JVM堆的初始大小和最大大小
  * @Author zhaochao
@@ -8,12 +10,13 @@ package org.fenixsoft.jvm;
  * @Version V1.0
  */
 public class HelloGC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // java虚拟机中的内存总量
         long totalMemory = Runtime.getRuntime().totalMemory();
         // java虚拟机试图使用的最大内存量
         long maxMemory = Runtime.getRuntime().maxMemory();
         System.out.println("TOTAL_MEMORY(-Xms)=" + totalMemory + "(字节)" + (totalMemory / 1024 / 2014) + "MB");
         System.out.println("MAX_MEMORY(-Xmx)=" + maxMemory + "(字节)" + (maxMemory / 1024 / 2014) + "MB");
+        TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
     }
 }

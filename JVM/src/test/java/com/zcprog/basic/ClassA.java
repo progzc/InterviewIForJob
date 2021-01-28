f4de07df41f0f4eb3191ae1765ee39a32e7d4025
progzc <zcprog@foxmail.com>
2021-01-28 17:16:09 +0800

    类的编译测试

---
+++ JVM/src/test/java/com/zcprog/basic/ClassA.java
@@ -0,0 +1,14 @@
+package com.zcprog.basic;
+
+/**
+ * @Description 类编译测试
+ * @Author zhaochao
+ * @Date 2021/1/28 17:14
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class ClassA {
+}
+
+class ClassB {
+}

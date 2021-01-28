73f388272f27d6c7cf5241a3625ad13f72870f31
progzc <zcprog@foxmail.com>
2021-01-28 17:12:25 +0800

    接口可实现多重继承

---
+++ JVM/src/test/java/com/zcprog/basic/CC.java
@@ -0,0 +1,20 @@
+package com.zcprog.basic;
+
+/**
+ * @Description TODO
+ * @Author zhaocho
+ * @Date 2021/1/28 17:11
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+interface AA {
+
+}
+
+interface BB {
+
+}
+
+public interface CC extends AA, BB {
+
+}

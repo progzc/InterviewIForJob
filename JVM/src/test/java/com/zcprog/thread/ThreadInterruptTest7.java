e12bc9fb64030667ed41eae0e247307ec18a42dc
progzc <zcprog@foxmail.com>
2021-01-29 18:35:35 +0800

    测试interrupted()方法

---
+++ JVM/src/test/java/com/zcprog/thread/ThreadInterruptTest7.java
@@ -0,0 +1,25 @@
+package com.zcprog.thread;
+
+/**
+ * @Description 测试interrupted()方法
+ * @Author zhaochao
+ * @Date 2021/1/29 18:33
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class ThreadInterruptTest7 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(100 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+        thread.interrupt();
+        Thread.currentThread().interrupt();
+        System.out.println("第一次调用返回值：" + Thread.interrupted()); // true
+        System.out.println("第二次调用返回值：" + Thread.interrupted()); // false
+    }
+}

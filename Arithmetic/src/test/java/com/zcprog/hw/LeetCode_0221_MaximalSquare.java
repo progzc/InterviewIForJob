9720c9220e119bbc0afe8d0f18d8f79b80733aa2
progzc <zcprog@foxmail.com>
2021-02-23 15:14:44 +0800

    最大正方形

---
+++ Arithmetic/src/test/java/com/zcprog/hw/LeetCode_0221_MaximalSquare.java
@@ -0,0 +1,43 @@
+package com.zcprog.hw;
+
+/**
+ * @Description 最大正方形
+ * @Author zhaochao
+ * @Date 2021/2/23 14:20
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class LeetCode_0221_MaximalSquare {
+    public static void main(String[] args) {
+        char[][] matrix = {{'1', '0', '1', '0', '0'},
+                {'1', '0', '1', '1', '1'},
+                {'1', '1', '1', '1', '1'},
+                {'1', '0', '0', '1', '0'}};
+        int maxSquare = maximalSquare(matrix);
+        System.out.println(maxSquare);
+    }
+
+    public static int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+}

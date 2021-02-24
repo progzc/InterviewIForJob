ec645fc986b69f0ec323b9d5776d0f60a8986277
progzc <zcprog@foxmail.com>
2021-02-24 19:01:41 +0800

    字符串相乘

---
+++ Arithmetic/src/test/java/com/zcprog/hw/LeetCode_0043_MultiplyStrings.java
@@ -0,0 +1,43 @@
+package com.zcprog.hw;
+
+/**
+ * @Description 字符串相乘
+ * @Author zhaochao
+ * @Date 2021/2/24 18:55
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class LeetCode_0043_MultiplyStrings {
+    public static void main(String[] args) {
+        String num1 = "123";
+        String num2 = "456";
+        System.out.println(multiply(num1, num2));
+    }
+
+    public static String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int m = num1.length();
+        int n = num2.length();
+        int[] ansArr = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            int x = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int y = num2.charAt(j) - '0';
+                ansArr[i + j + 1] += x * y;
+            }
+        }
+        for (int i = m + n - 1; i > 0; i--) {
+            ansArr[i - 1] += ansArr[i] / 10;
+            ansArr[i] %= 10;
+        }
+        int index = ansArr[0] == 0 ? 1 : 0;
+        StringBuffer ans = new StringBuffer();
+        while (index < m + n) {
+            ans.append(ansArr[index]);
+            index++;
+        }
+        return ans.toString();
+    }
+}

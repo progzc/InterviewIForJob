352ca1f29f18c3dc807c34c0487d0c815ac71fbd
progzc <zcprog@foxmail.com>
2021-02-27 14:31:48 +0800

    LeetCode0901-股票价格跨度-单调栈

--- Arithmetic/src/test/java/com/zcprog/hw/LeetCode_0901_OnlineStockSpan.java
+++ Arithmetic/src/test/java/com/zcprog/hw/LeetCode_0901_OnlineStockSpan.java
@@ -10,6 +10,15 @@ import java.util.Stack;
  * @Email zcprog@foxmail.com
  * @Version V1.0
  */
+
+/**
+ * 编写一个 StockSpanner 类，它收集某些股票的每日报价，并返回该股票当日价格的跨度。
+ * <p>
+ * 今天股票价格的跨度被定义为股票价格小于或等于今天价格的最大连续日数（从今天开始往回数，包括今天）。
+ * <p>
+ * 例如，如果未来7天股票的价格是 [100, 80, 60, 70, 60, 75, 85]，那么股票跨度将是 [1, 1, 1, 2, 1, 4, 6]。
+ * <p>
+ */
 public class LeetCode_0901_OnlineStockSpan {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);

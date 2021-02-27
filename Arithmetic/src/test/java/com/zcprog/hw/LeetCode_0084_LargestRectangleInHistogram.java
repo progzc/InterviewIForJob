37e713daf04077f641662850c8dbd2e519a67b53
progzc <zcprog@foxmail.com>
2021-02-27 12:07:54 +0800

    柱状图中最大的矩形

---
+++ Arithmetic/src/test/java/com/zcprog/hw/LeetCode_0084_LargestRectangleInHistogram.java
@@ -0,0 +1,39 @@
+package com.zcprog.hw;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * @Description 柱状图中最大的矩形
+ * @Author zhaochao
+ * @Date 2021/2/27 11:19
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class LeetCode_0084_LargestRectangleInHistogram {
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        System.out.println(largestRectangleArea(heights));
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+        Stack<Integer> mono_stack = new Stack<Integer>();
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                right[mono_stack.peek()] = i;
+                mono_stack.pop();
+            }
+            left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+            mono_stack.push(i);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+}

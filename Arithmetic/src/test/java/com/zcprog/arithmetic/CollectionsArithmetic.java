b93ed352756681099ac9cde90ee4daa08634f0ea
progzc <zcprog@foxmail.com>
2021-03-07 23:38:31 +0800

    Collections的正序/倒序/二分查找/洗牌算法/顺时针旋转/逆时针旋转

---
+++ Arithmetic/src/test/java/com/zcprog/arithmetic/CollectionsArithmetic.java
@@ -0,0 +1,97 @@
+package com.zcprog.arithmetic;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @Description Collections工具类提供的算法
+ * @Author zhaochao
+ * @Date 2021/3/7 22:32
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class CollectionsArithmetic {
+    @Test
+    public void sortPos() {
+        List<String> list = Arrays.asList("7", "4", "8", "3", "9");
+        // 升序
+        Collections.sort(list);
+        System.out.println(list); // [3, 4, 7, 8, 9]
+    }
+
+    @Test
+    public void sortNeg() {
+        List<String> list = Arrays.asList("7", "4", "8", "3", "9");
+        // 降序
+        Collections.sort(list, (String o1, String o2) -> {
+            return o2.compareTo(o1);
+        });
+        // 降序的另一种写法
+//        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println(list); // [9, 8, 7, 4, 3]
+    }
+
+    @Test
+    public void sortRevs() {
+        List<String> list = Arrays.asList("7", "4", "8", "3", "9");
+        // 降序的另一种写法
+        Collections.sort(list, Comparator.reverseOrder());
+        System.out.println(list); // [9, 8, 7, 4, 3]
+    }
+
+    @Test
+    public void sortBin() {
+        List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        // 二分排序
+        int index = Collections.binarySearch(list, "5");
+        System.out.println(index);
+    }
+
+    @Test
+    public void sortShuffle() {
+        List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        // 洗牌算法
+        Collections.shuffle(list);
+        System.out.println(list);
+    }
+
+    @Test
+    public void sortShuffle2() {
+        List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        // 洗牌算法
+        Collections.shuffle(list, new Random(100));
+        System.out.println(list);
+    }
+
+    @Test
+    public void sortShuffle3() {
+        List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        // 洗牌算法(自己实现)
+        Random random = new Random();
+        for (int i = list.size(); i > 1; i--) {
+            int ri = random.nextInt(i); // 随机位置
+            int ji = i - 1;
+            list.set(ji, list.set(ri, list.get(ji))); // 元素置换
+        }
+        System.out.println(list);
+    }
+
+    @Test
+    public void rotate() {
+        List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        // 顺时针旋转算法
+        Collections.rotate(list, 2);
+        System.out.println(list); // [8, 9, 1, 2, 3, 4, 5, 6, 7]
+    }
+
+    @Test
+    public void rotate2() {
+        List<String> list = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+        // 逆时针旋转算法
+        Collections.rotate(list, -2);
+        System.out.println(list); // [3, 4, 5, 6, 7, 8, 9, 1, 2]
+    }
+
+
+}

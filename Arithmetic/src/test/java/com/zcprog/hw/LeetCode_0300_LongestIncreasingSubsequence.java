dd83893d67ba778d0f150018ae0009965faa713a
progzc <zcprog@foxmail.com>
2021-03-21 10:04:04 +0800

    最长递增子序列-动态规划

---
+++ Arithmetic/src/test/java/com/zcprog/hw/LeetCode_0300_LongestIncreasingSubsequence.java
@@ -0,0 +1,32 @@
+package com.zcprog.hw;
+
+/**
+ * @Description 最长递增子序列
+ * @Author zhaochao
+ * @Date 2021/3/21 9:57
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class LeetCode_0300_LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        findLengthOfLIS(nums);
+    }
+
+    private static int findLengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int ans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}

2adb124ab6ce07950e78b325e283fa62cd324f93
progzc <zcprog@foxmail.com>
2021-03-21 19:06:29 +0800

    打印输出结果

--- Arithmetic/src/test/java/com/zcprog/hw/LeetCode_0300_LongestIncreasingSubsequence.java
+++ Arithmetic/src/test/java/com/zcprog/hw/LeetCode_0300_LongestIncreasingSubsequence.java
@@ -10,7 +10,7 @@ package com.zcprog.hw;
 public class LeetCode_0300_LongestIncreasingSubsequence {
     public static void main(String[] args) {
         int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
-        findLengthOfLIS(nums);
+        System.out.println(findLengthOfLIS(nums));
     }
 
     private static int findLengthOfLIS(int[] nums) {
